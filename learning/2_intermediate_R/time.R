baf4adb27c33247670fe778496f0635e312b8e28
Kristjan Kure <kritsjan.kure.1@gmail.com>
2021-11-29 16:28:54 +0200

    Updates to time file

--- learning/2_intermediate_R/time.R
+++ learning/2_intermediate_R/time.R
@@ -121,10 +121,10 @@ mean(day_diff)
 # -------------------------------------------------
 
 # Convert astro to vector of Date objects: astro_dates
-astro_dates <- as.Date(astro, format="%d-%b-%Y")
+astro_dates <- as.Date(astro, format = "%d-%b-%Y")
 
 # Convert meteo to vector of Date objects: meteo_dates
-meteo_dates <- as.Date(meteo, format="%B %d, %y")
+meteo_dates <- as.Date(meteo, format = "%B %d, %y")
 
 
 # Calculate the maximum absolute difference between astro_dates and meteo_dates
